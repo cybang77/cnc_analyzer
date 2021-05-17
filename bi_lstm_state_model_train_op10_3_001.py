65d993b7e63e871f6d7646808be3f4bb076d4699
dhkang <verdure0448@gmail.com>
2021-05-17 14:00:33 +0900

    init

---
+++ bi_lstm_state_model_train_op10_3_001.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import datetime
+import os
+import tensorflow as tf
+import hn.utile as hnutile
+# from keras_self_attention import SeqSelfAttention
+# from keras_self_attention import SeqSelfAttention
+
+# 루트 디렉토리
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# 모델 저장 루트 디렉토리명
+MODEL_ROOT_DIR_NAME = 'model'
+# 모델 저장 서브 디렉토리명
+MODEL_SUB_DIR_NAME = 'bi-lstm-state'
+# 모델파일 접두사
+MODEL_FILE_PREFIX = 'bi-lstm-state-model-'
+# 학습데이터 루트 디렉토리명
+TRAIN_ROOT_DIR_NAME = 'data'
+# 학습데이터 서브 디렉토리명
+TRAIN_SUB_DIR_NAME = 'roll'
+# 학습데이터 파일 접두사
+TRAIN_FILR_PREFIX = 'train-01-'
+# 학습데이터 경로
+CSV_FILE_PATH = os.path.join(ROOT_DIR, TRAIN_ROOT_DIR_NAME, TRAIN_SUB_DIR_NAME, TRAIN_FILR_PREFIX)
+
+# 학습, 예측 데이터 루프백 사이즈
+LOOK_BACK = 30
+
+FIT_PATIENCE = 80
+def run_trainning(model, fits, x_trains, y_trains, x_vals, y_vals):
+    '''
+    '''
+
+    custom_hist = hnutile.CustomEarlyStopCallback()
+
+    # model = tf.keras.models.load_model(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME, '074', 'bi-lstm-state-model-074-068-0.000107-0.000110.h5'))
+
+    for i in range(len(x_trains)):
+        custom_hist.init(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME), i, FIT_PATIENCE, MODEL_FILE_PREFIX)
+        # 모델 저장 폴더 생성
+        hnutile.MakeDir(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME,'%03d'%(i)))
+    
+        print('File %03dth: Start trainning.'%(i))
+        for j in range(fits):
+            custom_hist.set_fit_count(j)
+            print('Fit %d/%d.'%(j+1, fits))
+            hist = model.fit(x_trains[i], y_trains[i], epochs=1, batch_size=1, shuffle=False, callbacks=[custom_hist], validation_data=(x_vals[i%10], y_vals[i%10]))
+            model.reset_states()
+            # 조기 종료 체크
+            if custom_hist.early_stop():
+                break
+
+        # Bset model 선정
+        base_model_name = hnutile.FindBestModel(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME, '%03d'%(i)), MODEL_FILE_PREFIX)
+        if base_model_name != None:
+            print('File %03dth: The best model to be use is model >> %s.'%(i ,base_model_name))
+            model = tf.keras.models.load_model(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME, '%03d'%(i), base_model_name))
+        else:
+            print('File %03dth: Best model does not exist.'%(i))
+
+
+def create_bi_state_lstm_model(look_back=1):
+    '''
+    양방향 상태 전이 LSTM모델 생성
+    '''
+    model = tf.keras.Sequential()
+
+    for i in range(4):
+        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, batch_input_shape=(1, look_back, 1), stateful=True, return_sequences=True)))
+        model.add(tf.keras.layers.Dropout(0.3))
+
+    model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, stateful=True, return_sequences=False)))
+    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Dense(1))
+
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    return model
+
+def main():
+    '''
+    메인 함수
+    '''
+
+    datas = []
+    # train-01-001.csv ~ train-01-100.csv 까지 100개의 데이터 사용
+    for i in range(100):
+        file_path = "%s%.3d.csv"%(CSV_FILE_PATH, i+1)
+        datas.append(pd.read_csv(file_path))
+        print('%s 파일 데이터 추가.'%(file_path))
+
+    # 그래프 출력
+    hnutile.ShowGraph(datas, yaxis='RollLoad')
+
+    load_datas = []
+    for df in datas:
+        load_datas.append(df['RollLoad'].values[:, None])
+
+    # 학습, 검증 데이터 생성
+    x_trains, y_trains, x_vals, y_vals = hnutile.CreateTrainDatas(datas=load_datas, look_back=LOOK_BACK)
+
+    # 모델 생성
+    model = create_bi_state_lstm_model(LOOK_BACK)
+
+    run_trainning(model, 200,x_trains, y_trains, x_vals, y_vals)
+    print('Exit process.')
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ hn/utile.py
@@ -0,0 +1,143 @@
+
+import numpy as np
+from tensorflow.python.client import device_lib
+import plotly.graph_objects as go
+import plotly.io as pio
+import tensorflow as tf
+import os
+import re
+
+def GPU_INFO():
+    print(device_lib.list_local_devices())
+
+
+def create_dataset(signal_data, look_back=1):
+    '''
+    룩백사이즈로 데이터 생성
+     dataX 루백사이즈 데이터 array
+     dataY 루백+1번째 데이터 array(1건)
+    '''
+    dataX, dataY = [], []
+    for i in range(len(signal_data)-look_back):
+        dataX.append(signal_data[i:(i+look_back), 0])
+        dataY.append(signal_data[i + look_back, 0])
+    return np.array(dataX), np.array(dataY)
+
+def CreateTrainDatas(datas, look_back=1):
+    '''
+    '''
+    x_trains = []
+    y_trains = []
+    x_vals = []
+    y_vals = []
+
+    for i in range(90):
+        x_train, y_train = create_dataset(datas[i], look_back)
+        x_trains.append(x_train)
+        y_trains.append(y_train)
+    
+    for i in range(10):
+        x_val, y_val = create_dataset(datas[i+90], look_back)
+        x_vals.append(x_val)
+        y_vals.append(y_val)
+
+    # 입력형 변환
+    for i in range(len(x_trains)):
+        x_trains[i] = np.reshape(x_trains[i], (x_trains[i].shape[0], x_trains[i].shape[1], 1))
+
+    for i in range(len(x_vals)):
+        x_vals[i] = np.reshape(x_vals[i], (x_vals[i].shape[0], x_vals[i].shape[1], 1))
+
+    return x_trains, y_trains, x_vals, y_vals
+
+
+def ShowGraph(datas, yaxis='Load'):
+    pio.templates.default = "plotly_dark"
+   
+    fig = go.Figure(layout=go.Layout(height=600, width=1200))
+    cnt = 0
+    for df in datas:
+        fig = fig.add_trace(go.Scatter(y=df[yaxis], mode='lines', name = yaxis + str(cnt), line=dict(width=1)))
+        cnt += 1
+
+    fig.update_layout(title=yaxis,
+                    xaxis_title='x',
+                    yaxis_title='y')
+    fig.show()
+
+
+def FindBestModel(path_dir, file_pre_fix):
+    regex = re.compile(r'^%s(\d{3})-(\d{3})-(\d.\d{6})-(\d.\d{6}).h5'%(file_pre_fix))
+    file_list = os.listdir(path_dir)
+    val_loss_list = []
+    for i in range(len(file_list)):
+        name = file_list[i]
+        matchobj = regex.search(name)
+        if matchobj != None:
+            val_loss_list.append(float(matchobj.group(4)))
+
+    # print(val_loss_list)
+    if len(val_loss_list) > 0:
+        return file_list[val_loss_list.index(min(val_loss_list))]
+    return None
+
+def MakeDir(path):
+    '''
+    '''
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+
+class CustomEarlyStopCallback(tf.keras.callbacks.Callback):
+    '''
+    '''
+    def init(self, model_dir, file_count, patience=10, file_pre_fix=None):
+        self.loss = []
+        self.val_loss = []
+        self.model_dir = model_dir
+        self.file_count = file_count
+        self.patience = patience
+        self.wait = 0
+        self.best_val = np.Inf
+        self.file_pre_fix = file_pre_fix
+    def early_stop(self):
+        if self.wait >= self.patience:
+            print('\nFit %05d: early stopping'%(self.file_count)) 
+            return True
+        return False
+
+    def set_fit_count(self, fit_count):
+        self.fit_count = fit_count
+
+    def on_epoch_end(self, batch, logs={}):
+   
+        if len(self.val_loss) > 0:
+            min_val_loss = min(self.val_loss)
+
+            if min_val_loss > logs.get('val_loss'): # 성능개선이 된 경우
+                # save
+                filename = os.path.join(
+                    self.model_dir,
+                    '%03d'%(self.file_count),
+                    '%s%03d-%03d-%.6f-%.6f.h5'%(self.file_pre_fix, self.file_count, self.fit_count, logs.get('loss'), logs.get('val_loss'))
+                    )
+                self.model.save(filename)
+                self.best_val = logs.get('val_loss')
+                self.wait = 0
+                print('Fit %05d: val_loss improved from %.6f to %.6f, saving model to %s'%(self.fit_count, min_val_loss, logs.get('val_loss'), filename))
+            else: # 성능개선이 없는경우
+                self.wait +=1
+                print('Fit %05d: val_loss did not improve from %.6f'%(self.fit_count, min_val_loss))
+        else: # 학습 처음시작인경우
+            filename = os.path.join(
+                    self.model_dir,
+                    '%03d'%(self.file_count),
+                    '%s%03d-%03d-%.6f-%.6f.h5'%(self.file_pre_fix, self.file_count, self.fit_count, logs.get('loss'), logs.get('val_loss'))
+                    )
+            self.model.save(filename)
+            self.best_val = logs.get('val_loss')
+            self.wait = 0
+            print('Fit %05d: val_loss improved from inf to %.6f, saving model to %s'%(self.fit_count, logs.get('val_loss'), filename))
+        self.loss.append(logs.get('loss'))
+        self.val_loss.append(logs.get('val_loss'))
