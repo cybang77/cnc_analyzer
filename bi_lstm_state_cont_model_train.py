31955d0e55587cdc67b56d83b6c6974a1a903243
dhkang <verdure0448@gmail.com>
2021-05-18 15:42:55 +0900

    연속데이터 처리 추가

---
+++ bi_lstm_state_cont_model_train.py
@@ -0,0 +1,124 @@
+import numpy as np
+import pandas as pd
+import datetime
+import os
+import tensorflow as tf
+import hn.utile as hnutile
+import plotly.io as pio
+import plotly.graph_objects as go
+# from keras_self_attention import SeqSelfAttention
+# from keras_self_attention import SeqSelfAttention
+
+# 루트 디렉토리
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# 모델 저장 루트 디렉토리명
+MODEL_ROOT_DIR_NAME = 'model'
+# 모델 저장 서브 디렉토리명
+MODEL_SUB_DIR_NAME = 'bi-lstm-state-cont'
+# 모델파일 접두사
+MODEL_FILE_PREFIX = 'bi-lstm-state-cont-model-'
+# 학습데이터 루트 디렉토리명
+TRAIN_ROOT_DIR_NAME = 'data'
+# 학습데이터 서브 디렉토리명
+TRAIN_SUB_DIR_NAME = 'cont-roll'
+# 학습데이터 파일 접두사
+TRAIN_FILR_PREFIX = 'train-01-'
+# 학습데이터 경로
+CSV_FILE_PATH = os.path.join(ROOT_DIR, TRAIN_ROOT_DIR_NAME, TRAIN_SUB_DIR_NAME)
+
+# 학습, 예측 데이터 루프백 사이즈
+LOOK_BACK = 50
+
+FIT_PATIENCE = 80
+LEARNING_RATE = 0.00001
+def run_trainning(model, fits, x_trains, y_trains, x_vals, y_vals):
+    '''
+    학습 자동화
+     모델 학습 및 저장, 조기학습종료, 베스트 모델선정 등
+    '''
+
+    custom_hist = hnutile.CustomEarlyStopCallback()
+
+    for i in range(len(x_trains)):
+        custom_hist.init(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME), i, FIT_PATIENCE, MODEL_FILE_PREFIX)
+        # 모델 저장 폴더 생성
+        hnutile.MakeDir(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME,'%03d'%(i)))
+    
+        print('File %03dth: Start trainning.'%(i))
+        for j in range(fits):
+            custom_hist.set_fit_count(j)
+            print('Fit %d/%d.'%(j+1, fits))
+            hist = model.fit(x_trains[i], y_trains[i], epochs=1, batch_size=1, shuffle=False, callbacks=[custom_hist], validation_data=(x_vals[i], y_vals[i]))
+            model.reset_states()
+            # 조기 종료 체크
+            if custom_hist.early_stop():
+                break
+
+        # Bset model 선정
+        base_model_name = hnutile.FindBestModel(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME, '%03d'%(i)), MODEL_FILE_PREFIX)
+        if base_model_name != None:
+            print('File %03dth: The best model to be use is model >> %s.'%(i ,base_model_name))
+            model = tf.keras.models.load_model(os.path.join(ROOT_DIR, MODEL_ROOT_DIR_NAME, MODEL_SUB_DIR_NAME, '%03d'%(i), base_model_name), compile=False)
+            model.compile(loss=root_mean_squared_error, optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE))
+        else:
+            print('File %03dth: Best model does not exist.'%(i))
+
+def root_mean_squared_error(y_true, y_pred):
+    return tf.math.sqrt(tf.keras.losses.mean_squared_error(y_true, y_pred))
+
+def create_bi_state_lstm_model(look_back=1):
+    '''
+    양방향 상태 전이 LSTM모델 생성
+    '''
+    model = tf.keras.Sequential()
+
+    for i in range(3):
+        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, batch_input_shape=(1, look_back, 1), stateful=True, return_sequences=True)))
+        model.add(tf.keras.layers.Dropout(0.3))
+
+    model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512, stateful=True, return_sequences=False)))
+    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Dense(1))
+
+    # model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate=0.000001))
+    model.compile(loss=root_mean_squared_error, optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001))
+    return model
+
+def main():
+    '''
+    메인 함수
+    '''
+
+    df = pd.read_csv(os.path.join(CSV_FILE_PATH ,'op10-3-train-01.csv'))
+    # 그래프 출력
+    hnutile.ShowContinueGraph(df, yaxis='RollLoad')
+    
+    load_datas = df['RollLoad'].values[:, None]
+
+    # 모델 생성
+    model = create_bi_state_lstm_model(LOOK_BACK)
+
+    
+    # x_train, y_train, x_val, y_val = hnutile.CreateContinuTrainDatas(data=load_datas[:10000], look_back=LOOK_BACK)
+
+    data_fit_size= 12000
+    data_fit_count = int(len(load_datas) / data_fit_size)
+    x_trains = []
+    y_trains = []
+    x_vals = []
+    y_vals = []
+    for i in range(data_fit_count):
+        x_train, y_train, x_val, y_val = hnutile.CreateContinuTrainDatas(data=load_datas[i*data_fit_size:(i+1)*data_fit_size], look_back=LOOK_BACK)
+        x_trains.append(x_train)
+        y_trains.append(y_train)
+        x_vals.append(x_val)
+        y_vals.append(y_val)
+
+    run_trainning(model, 200, x_trains, y_trains, x_vals, y_vals)
+
+    print('Exit process.')
+
+
+if __name__ == '__main__':
+    main()
+    
--- hn/utile.py
+++ hn/utile.py
@@ -23,6 +23,17 @@ def create_dataset(signal_data, look_back=1):
         dataY.append(signal_data[i + look_back, 0])
     return np.array(dataX), np.array(dataY)
 
+def CreateContinuTrainDatas(data, look_back=1, train_ratio=0.9):
+    train_size = int(len(data) * train_ratio)
+ 
+    x_train, y_train = create_dataset(data[:train_size])
+    x_val, y_val = create_dataset(data[train_size:])
+
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], 1))
+
+    return x_train, y_train, x_val, y_val
+
 def CreateTrainDatas(datas, look_back=1):
     '''
     '''
@@ -50,6 +61,15 @@ def CreateTrainDatas(datas, look_back=1):
 
     return x_trains, y_trains, x_vals, y_vals
 
+def ShowContinueGraph(data, yaxis='Load'):
+    pio.templates.default = "plotly_dark"
+    fig = go.Figure(layout=go.Layout(height=600, width=1200))
+    fig = fig.add_trace(go.Scatter(y=data[yaxis], mode='lines', name = yaxis, line=dict(width=1)))
+
+    fig.update_layout(title=yaxis,
+                    xaxis_title='x',
+                    yaxis_title='y')
+    fig.show()
 
 def ShowGraph(datas, yaxis='Load'):
     pio.templates.default = "plotly_dark"
@@ -92,7 +112,7 @@ def MakeDir(path):
 class CustomEarlyStopCallback(tf.keras.callbacks.Callback):
     '''
     '''
-    def init(self, model_dir, file_count, patience=10, file_pre_fix=None):
+    def init(self, model_dir, file_count, patience=10, file_pre_fix=None, best_model_save=True):
         self.loss = []
         self.val_loss = []
         self.model_dir = model_dir
@@ -101,9 +121,11 @@ class CustomEarlyStopCallback(tf.keras.callbacks.Callback):
         self.wait = 0
         self.best_val = np.Inf
         self.file_pre_fix = file_pre_fix
+        self.best_model_save = best_model_save
+
     def early_stop(self):
         if self.wait >= self.patience:
-            print('\nFit %05d: early stopping'%(self.file_count)) 
+            print('Fit %05d: early stopping'%(self.file_count)) 
             return True
         return False
 
@@ -114,21 +136,25 @@ class CustomEarlyStopCallback(tf.keras.callbacks.Callback):
    
         if len(self.val_loss) > 0:
             min_val_loss = min(self.val_loss)
-
-            if min_val_loss > logs.get('val_loss'): # 성능개선이 된 경우
-                # save
-                filename = os.path.join(
+            
+            filename = os.path.join(
                     self.model_dir,
                     '%03d'%(self.file_count),
                     '%s%03d-%03d-%.6f-%.6f.h5'%(self.file_pre_fix, self.file_count, self.fit_count, logs.get('loss'), logs.get('val_loss'))
                     )
+            if min_val_loss > logs.get('val_loss'): # 성능개선이 된 경우
+                # save
                 self.model.save(filename)
                 self.best_val = logs.get('val_loss')
                 self.wait = 0
                 print('Fit %05d: val_loss improved from %.6f to %.6f, saving model to %s'%(self.fit_count, min_val_loss, logs.get('val_loss'), filename))
             else: # 성능개선이 없는경우
                 self.wait +=1
-                print('Fit %05d: val_loss did not improve from %.6f'%(self.fit_count, min_val_loss))
+                if self.best_model_save == False:
+                    self.model.save(filename)
+                    print('Fit %05d: val_loss did not improve from %.6f, wait %d/%d, saving model to %s'%(self.fit_count, min_val_loss, self.wait, self.patience, filename))
+                else:
+                    print('Fit %05d: val_loss did not improve from %.6f, wait %d/%d'%(self.fit_count, min_val_loss, self.wait, self.patience))
         else: # 학습 처음시작인경우
             filename = os.path.join(
                     self.model_dir,
